db47fecd64f862a66bf64be7d0f9decee44bd817
xpwu <diodexp@foxmail.com>
2026-09-24 16:20:46 +0800

    delete unused db

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.25.0
 
 require (
 	github.com/stretchr/testify v1.11.1
-	github.com/syndtr/goleveldb v1.0.0
 	github.com/xpwu/go-cmd v0.5.0
 	github.com/xpwu/go-config v0.1.0
 	github.com/xpwu/go-httpclient v0.1.4
--- mongodb/config.go
+++
@@ -1,8 +0,0 @@
-package mongodb
-
-import "github.com/xpwu/go-mongodb/client"
-
-type serverWorkingDB struct {
-	Deployment client.Config `conf:",Server working db. MongoDB when URI is set; LevelDB otherwise"`
-	DB         string
-}
